7115cd2f71de3f145bb0dbcd32bbf903d75424ac
Amitkanswal <amit.kanswal@contentstack.com>
2022-05-26 12:43:29 +0530

    fix:resolved github login issue [ECO-748]

--- tests/pages/github-login.ts
+++ tests/pages/github-login.ts
@@ -1,9 +1,12 @@
 import { expect, Page } from "@playwright/test";
+import totp from "totp-generator";
 
 export class GithubLogin {
     readonly page;
+    twoFAtoken: string;
     constructor(page: Page) {
         this.page = page
+        // this.twoFAtoken = totp(process.env.SECRET_KEY)
     }
 
     async getGithubPage() {
@@ -17,6 +20,11 @@ export class GithubLogin {
         await this.page.locator('#login_field').type(process.env.GITHUB_LOGIN_ID);
         await this.page.locator('#password').type(process.env.GITHUB_PASSWORD);
         await this.page.locator('[data-signin-label="Sign in"]').click();
+        await this.page.waitForTimeout(1000);
+        const twoFAToken = await totp(process.env.SECRET_KEY)
+        await this.page.locator('.js-verification-code-input-auto-submit').fill(twoFAToken);
+        await this.page.waitForTimeout(1000);
+        expect(this.page.url()).toContain('github.com');
     }
 
 }

8b9ac351b36cbe8f95e6e32783c2d9fe22f8ba6c
keriwilkins <keri.wilkins@motivcx.com>
2024-01-31 13:22:07 -0600

    ran build
    ran build now working fully with content on the npm start

--- src/pages/error.tsx
+++ src/pages/error.tsx
@@ -5,6 +5,7 @@ export default function Error() {
     <div className='error-page'>
       <div className='error-message'>
         <h1>404: Not Found</h1>
+        <h1>Testing that this route is working</h1>
         <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
       </div>
     </div>

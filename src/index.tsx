e40ab285b24bd6320031bf1cf147cfd3ac0e4765
keriwilkins <keri.wilkins@motivcx.com>
2024-02-01 11:18:57 -0600

    adding domcontentload function to the tsx

--- src/index.tsx
+++ src/index.tsx
@@ -6,8 +6,8 @@ import App from "./App";
 import { LivePreviewProvider } from "./context/live-preview-context-provider";
 import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
+document.addEventListener('DOMContentLoaded', () => {
+  const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement
 );
 root.render(
   <BrowserRouter>
@@ -16,6 +16,7 @@ root.render(
     </LivePreviewProvider>
   </BrowserRouter>,
 );
+});
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
